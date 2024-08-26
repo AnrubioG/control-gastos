294b73051dd34ecea572cd0e2f23f0b36d93c5c5
AnubioG <angela.rubio@genstudents.org>
2024-08-26 18:15:16 -0500

    Calcular progreso y reiniciar aplicación

--- src/components/BudgetTracker.tsx
+++ src/components/BudgetTracker.tsx
@@ -1,22 +1,43 @@
+import { CircularProgressbar, buildStyles } from "react-circular-progressbar";
+import { useBudget } from "../hooks/use-budget";
 import AmountDisplay from "./AmountDisplay";
+import "react-circular-progressbar/dist/styles.css";
 
 export default function BudgetTracker() {
+  const { state, totalExpenses, remainingBudget, dispatch } = useBudget();
+  const percentage = +((totalExpenses / state.budget) * 100).toFixed(2);
+
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 gap-5">
       <div className="flex justify-center">
-        <img src="/img/grafico.jpg" alt="Grafico de Gastos" />
+        <CircularProgressbar
+          value={percentage}
+          styles={buildStyles({
+            pathColor:
+              percentage > 85
+                ? "#D42E1C"
+                : percentage > 60
+                ? "#FD660B"
+                : "#3b82f6",
+            trailColor: "#f5f5f5",
+            textSize: 8,
+          })}
+          text={`${percentage}% gastado`}
+          strokeWidth={15}
+        />
       </div>
 
       <div className=" flex flex-col justify-center items-center gap-8">
         <button
           type="button"
           className="bg-pink-500 w-full p-2 text-white uppercase font-bold rounded-lg"
+          onClick={() => dispatch({ type: "reset-app" })}
         >
           Resetear App
         </button>
-        <AmountDisplay label="Presupuesto" amount={3000000} />
-        <AmountDisplay label="Presupuesto" amount={200} />
-        <AmountDisplay label="Presupuesto" amount={100} />
+        <AmountDisplay label="Presupuesto" amount={state.budget} />
+        <AmountDisplay label="Disponible" amount={remainingBudget} />
+        <AmountDisplay label="Gastado" amount={totalExpenses} />
       </div>
     </div>
   );
--- src/components/ExpenseForm.tsx
+++ src/components/ExpenseForm.tsx
@@ -16,8 +16,9 @@ export default function ExpenseForm() {
   });
 
   const [error, setError] = useState("");
+  const [previousAmount, setPreviousAmount] = useState(0);
 
-  const { dispatch, state } = useBudget();
+  const { dispatch, state, remainingBudget } = useBudget();
 
   useEffect(() => {
     if (state.editingId) {
@@ -25,6 +26,7 @@ export default function ExpenseForm() {
         (currentExpense) => currentExpense.id === state.editingId
       )[0];
       setExpense(editingExpense);
+      setPreviousAmount(editingExpense.amount);
     }
   }, [state.editingId]);
 
@@ -55,6 +57,12 @@ export default function ExpenseForm() {
       return;
     }
 
+    // validar que no se supere el presepuesto disponible
+    if (expense.amount - previousAmount > remainingBudget) {
+      setError("Se ha superado el presupuesto disponible");
+      return;
+    }
+
     // Agregar o actualizar gasto
     if (state.editingId) {
       dispatch({
@@ -64,6 +72,16 @@ export default function ExpenseForm() {
     } else {
       dispatch({ type: "add-expense", payload: { expense } });
     }
+
+    // reiniciar el state
+    setExpense({
+      amount: 0,
+      expenseName: "",
+      category: "",
+      date: new Date(),
+    });
+
+    setPreviousAmount(0);
   };
 
   return (
